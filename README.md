d17724121f6e6fba1568e5852c4c06c2ad54de20
theo-jedi <feds.msc@gmail.com>
2023-05-11 02:56:04 +0300

    Add website

---
+++ README.md
@@ -0,0 +1,4 @@
+# Neotive
+A team of software development professionals creating mobile applications using cutting-edge technologies. Our goal is to make products that improve everyone's life
+
+[neotive.github.io](https://neotive.github.io)
---
+++ scripts/resources.js
@@ -0,0 +1,56 @@
+/** Main **/
+export const text_main_secondary_button = "Google Play"
+export const text_main_secondary_button_link = "https://play.google.com/store/apps/dev?id=5083558951092887752"
+export const text_main_primary_button = "Support the development"
+export const text_main_primary_button_link = "https://bmc.link/neotive"
+
+export const image_main_background = "url(../images/background.jpg)"
+export const image_main_logo = "../images/logo.png"
+
+export const button_github_link = "https://github.com/neotive"
+export const button_github_icon = "fab fa-github"
+export const button_github_size = "20"
+
+export const button_bmc_link = "https://bmc.link/neotive"
+export const button_bmc_icon = "fab fa-patreon"
+
+export const button_telegram_link = "https://t.me/theojarrus"
+export const button_telegram_icon = "fab fa-telegram-plane"
+export const button_telegram_size = "20"
+
+export const button_appstore_link = "https://apple.com/app-store"
+export const button_appstore_icon = "fas fa-apple-alt"
+export const button_appstore_size = "17"
+
+export const button_email_link = "mailto:neotivesoftware@gmail.com"
+export const button_email_icon = "fas fa-envelope"
+export const button_email_size = "17"
+
+/** Footer **/
+export const text_footer_divider = "\u00A0/\u00A0"
+export const text_footer_copyrights = "© Neotive "
+export const text_footer_tagline = "bring ideas to live better"
+export const text_footer_privacy_policy = "privacy policy"
+export const text_footer_privacy_link = "../privacy.html"
+export const text_footer_terms = "terms"
+export const text_footer_terms_link = "../terms.html"
+
+/** About **/
+export const text_about_title = "About us"
+export const text_about_description = "A team of software development professionals creating mobile applications using cutting-edge technologies. Our goal is to make products that improve everyone's life"
+
+/** Apps **/
+export const text_apps_title = "Our apps"
+
+export const text_button_available = "Download"
+export const text_button_unavailable = "Unavailable"
+
+export const app_wavenote_name = "Wavenote"
+export const app_wavenote_description = "Incredible notebook for musicians. Open up new possibilities and simplify your daily tasks with editor, studio, chords, metronome and other features."
+export const app_wavenote_link = "https://play.google.com/store/apps/details?id=com.theost.wavenote"
+export const app_wavenote_image = "../images/wavenote.png"
+
+export const app_tike_name = "Tike"
+export const app_tike_description = "Powerful social network built around your schedule. Allows to easily plan your own and other people's time with smart shared events."
+export const app_tike_link = "https://github.com/theo-jedi/tike-android/releases"
+export const app_tike_image = "../images/tike.png"
